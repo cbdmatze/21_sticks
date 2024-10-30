76beb71d25ff23e45132f32302f4c0b829b1c409
cbdmatze <mrosanowitsch@live.de>
2024-10-30 07:33:38 +0100

    first commit

---
+++ 21_sticks.py
@@ -0,0 +1,8 @@
+def play_game():
+    """
+    Starts the game of 21 sticks for two players.
+    
+    Each player takes turns removing 1-3 sticks from the pile.
+    The Player who takes the last sick loses. The game continues 
+    until there are no more sticks left, and the winner is declared.
+    """
